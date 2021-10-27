fb79d6ebb2ea1a8349892e7708d3a055fc75d08d
Luceta <57941164+Luceta@users.noreply.github.com>
2021-10-27 17:02:00 +0900

    ADD:webpack config

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use("/uploads", express.static("uploads"));
+app.use("/static", express.static("assets"));
 
 app.use(
   session({
